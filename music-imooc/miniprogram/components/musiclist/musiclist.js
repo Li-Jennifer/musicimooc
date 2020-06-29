f8c5e97f2f59049d8937189320de8f7029ec9f18
今天会有好事发生 <fjiaolee@163.com>
2020-06-29 23:40:57 +0800

    musicplayer

--- music-imooc/miniprogram/components/musiclist/musiclist.js
+++ music-imooc/miniprogram/components/musiclist/musiclist.js
@@ -1,23 +1,30 @@
 // components/musiclist/musiclist.js
+const app = getApp()
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
-
+    musiclist: Array
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    playingId:-1
   },
-
-  /**
-   * 组件的方法列表
-   */
+  pageLifetimes:{
+    show(){
+      // console.log(app.getPlayMusicId())
+      this.setData({
+        playingId:parseInt(app.getPlayMusicId())
+      })
+    }
+  },
   methods: {
-
+    onSelect(event){
+      const ds = event.currentTarget.dataset
+      const musicid = ds.musicid
+      this.setData({
+        playingId: musicid
+      })
+      wx.navigateTo({
+        url: `../../pages/player/player?musicId=${musicid}&index=${ds.index}`,
+      })
+    }
   }
 })
